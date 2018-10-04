73dfa381fc77dacb8ed3b8e17cfeda2af59be637
Kayla Washburn <partheseas@gmail.com>
2018-10-04 10:18:42 -0600

    Auto scrolling on mobile is a bad nightmare

--- resources/index.js
+++ resources/index.js
@@ -1,45 +1,47 @@
 // This variable is used to fix the terrible awful way that iOS handles
 // the shrinking address bar when you scroll down on a page.
-let previousFrameSize = null
+let mobile = navigator.userAgent.toLowerCase().includes( 'mobile' )
+let wideEnough = window.innerWidth > 800
 let autoScrolling = false
 let panel = null
 
-$( document ).ready( () => {
-  $( window ).keydown( event => {
-    if ( !autoScrolling ) {
-      switch ( event.keyCode ) {
-        case 74:
-        case 40:
-          startAutoScrolling( ++panel )
-          break;
-        case 75:
-        case 38:
-          startAutoScrolling( --panel )
-          break;
+if ( !mobile ) {
+  $( document ).ready( () => {
+    $( window ).keydown( event => {
+      if ( !autoScrolling ) {
+        switch ( event.keyCode ) {
+          case 74:
+          case 40:
+            startAutoScrolling( ++panel )
+            break;
+          case 75:
+          case 38:
+            startAutoScrolling( --panel )
+            break;
+        }
+      }
+    })
+  })
+
+  $( document ).scroll( () => {
+    setPanelValue()
+
+    if ( !autoScrolling && wideEnough ) {
+      if ( window.scrollY > panel * window.innerHeight + 100 ) {
+        startAutoScrolling( ++panel )
+      } else if ( window.scrollY < panel * window.innerHeight - 100 ) {
+        startAutoScrolling( --panel )
       }
     }
   })
-})
 
-$( document ).scroll( () => {
-  setPanelValue()
-
-  if ( !autoScrolling && window.innerHeight === previousFrameSize ) {
-    if ( window.scrollY > panel * window.innerHeight + 100 ) {
-      startAutoScrolling( ++panel )
-    } else if ( window.scrollY < panel * window.innerHeight - 100 ) {
-      startAutoScrolling( --panel )
+  $( window ).resize( () => {
+    wideEnough = window.innerWidth > 800
+    if ( Math.abs( window.scrollY - panel * window.innerHeight ) > 100 ) {
+      startAutoScrolling( panel )
     }
-  }
-
-  previousFrameSize = window.innerHeight
-})
-
-$( window ).resize( () => {
-  if ( Math.abs( window.scrollY - panel * window.innerHeight ) > 100 ) {
-    startAutoScrolling( panel )
-  }
-})
+  })
+}
 
 function setPanelValue() {
   panel = Math.round( window.scrollY / window.innerHeight )
