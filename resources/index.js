72a2b122ba4133070474162c6b03b7caa2db44ee
Kayla Washburn <partheseas@gmail.com>
2018-10-03 23:11:49 -0600

    Brand spankin' new!

---
+++ resources/index.js
@@ -0,0 +1,58 @@
+// This variable is used to fix the terrible awful way that iOS handles
+// the shrinking address bar when you scroll down on a page.
+let previousFrameSize = null
+let autoScrolling = false
+let panel = null
+
+$( document ).ready( () => {
+  $( window ).keydown( event => {
+    if ( !autoScrolling ) {
+      switch ( event.keyCode ) {
+        case 74:
+        case 40:
+          startAutoScrolling( ++panel )
+          break;
+        case 75:
+        case 38:
+          startAutoScrolling( --panel )
+          break;
+      }
+    }
+  })
+})
+
+$( document ).scroll( () => {
+  if ( panel === null ) {
+    setPanelValue()
+    previousFrameSize = window.innerHeight
+  }
+
+  if ( !autoScrolling && window.innerHeight === previousFrameSize ) {
+    if ( window.scrollY > panel * window.innerHeight + 100 ) {
+      startAutoScrolling( ++panel )
+    } else if ( window.scrollY < panel * window.innerHeight - 100 ) {
+      startAutoScrolling( --panel )
+    }
+  }
+
+  previousFrameSize = window.innerHeight
+  setPanelValue()
+})
+
+$( window ).resize( () => {
+  if ( Math.abs( window.scrollY - panel * window.innerHeight ) > 100 ) {
+    startAutoScrolling( panel )
+  }
+})
+
+function setPanelValue() {
+  panel = Math.round( window.scrollY / window.innerHeight )
+}
+
+function startAutoScrolling( toPanel ) {
+  autoScrolling = true
+  $( window ).scrollTo( toPanel * window.innerHeight, 1000, { easing: 'easeOutExpo', onAfter: () => {
+    setPanelValue()
+    setTimeout( () => autoScrolling = false, 100 )
+  }} )
+}
